4b4a49d105ed0f8a6a53d6c91939139492bed4c8
Rasmus Lindahl <mrconter1@gmail.com>
2024-03-25 15:07:54 +0100

    Update name again

--- human.py
+++ human.py
@@ -7,7 +7,7 @@ class Human(ResourceContainer):
                 'oxygen': 550,
                 'calories': 2500,
                 'water': 3.7,
-                'living_area': 100,
+                'volume': 100,
             },
             provides ={
                 'co2': 1,
