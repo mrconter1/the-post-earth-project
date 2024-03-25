8a364a99c348a52970b971fb8b04752b8386f5fe
Rasmus Lindahl <mrconter1@gmail.com>
2024-03-25 14:50:45 +0100

    Improve formatting of different modules

--- human.py
+++ human.py
@@ -3,6 +3,15 @@ from resource_container import ResourceContainer
 class Human(ResourceContainer):
     def __init__(self):
         super().__init__(
-            inputs={'oxygen': 550, 'calories': 2500, 'water': 3.7},
-            outputs={'co2': 1, 'organic_waste': 0.5, 'wastewater': 2.5}
+            inputs={
+                'oxygen': 550,
+                'calories': 2500,
+                'water': 3.7,
+                'living_space': 100,
+            },
+            outputs={
+                'co2': 1,
+                'organic_waste': 0.5,
+                'wastewater': 2.5,
+            }
         )
--- space_habitat.py
+++ space_habitat.py
@@ -2,7 +2,12 @@ from resource_container import ResourceContainer
 from human import Human 
 
 class Habitat(ResourceContainer):
-    pass
+    def __init__(self):
+        super().__init__(
+            outputs={
+                'living_space': 500
+            }
+        )
 
 # Create 'Human' instances
 human1 = Human()
@@ -13,10 +18,6 @@ space_habitat = Habitat()
 space_habitat.add_container(human1)
 space_habitat.add_container(human2)
 
-# Create a nested container if needed
-nested_habitat = Habitat()
-nested_habitat.add_container(space_habitat)  
-
 # Get a summary of total daily resources for the habitat
-print(f"Total daily consumption for the habitat: {nested_habitat.get_total_consumption()}")
-print(f"Total daily production for the habitat: {nested_habitat.get_total_production()}")
+print(f"Total daily consumption for the habitat: {space_habitat.get_total_consumption()}")
+print(f"Total daily production for the habitat: {space_habitat.get_total_production()}")
