022485610801c78e365c589a02c7452062de7747
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-03-26 18:46:12 +0100

    Add route for organic waste rest-overs from plant

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ world_engine.add_entity(waste_facility_station)
 #world_engine.create_resource_route('co2', habitat, farming_station)
 
 #world_engine.create_resource_route('organic_waste', habitat, waste_facility_station)
+#world_engine.create_resource_route('organic_waste', farming_station, waste_facility_station)
 #world_engine.create_resource_route('nutrients', waste_facility_station, farming_station)
 
 #world_engine.create_resource_route('water', water_mining_station, habitat)
