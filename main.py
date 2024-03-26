ae8405ed9b67770129f7bd1a3af8d94056e227b7
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-03-26 12:05:30 +0100

    Restructure comments

--- main.py
+++ main.py
@@ -7,15 +7,9 @@ def generate_habitat_station_instance():
     habitat = HabitatStation()
 
     # Adding monthly requirements for 700 people
-
-    # Oxygen: 550 liters/day * 30 days * 700 people
-    habitat.add_to_stockpile('oxygen', 550 * 30 * 700)
-
-    # Water: 3.7 liters/day * 30 days * 700 people
-    habitat.add_to_stockpile('water', 3.7 * 30 * 700)
-
-    # Calories: 2500 kcal/day * 30 days * 700 people
-    habitat.add_to_stockpile('calories', 2500 * 30 * 700)
+    habitat.add_to_stockpile('oxygen', 550 * 30 * 700)      # 550 liters/day * 30 days * 700 people
+    habitat.add_to_stockpile('water', 3.7 * 30 * 700)       # 3.7 liters/day * 30 days * 700 people
+    habitat.add_to_stockpile('calories', 2500 * 30 * 700)   # 2500 kcal/day * 30 days * 700 people
 
     return habitat
 
