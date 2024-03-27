d58ddd15dc69cf9272656b2a74ba1ddf54a83838
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-03-27 11:08:23 +0100

    Add ability to add multiple entities at the same time to the world engine

--- models/world_engine.py
+++ models/world_engine.py
@@ -5,6 +5,14 @@ class WorldEngine:
     def add_entity(self, entity):
         self.entities.append(entity)
 
+    def add_entities(self, *entities):
+        """
+        Adds multiple entities to the world. Entities can be passed as separate arguments.
+        """
+        for entity in entities:
+            self.add_entity(entity)
+        return self
+
     def tick(self):
         # Update resources for each entity
         for entity in self.entities:
