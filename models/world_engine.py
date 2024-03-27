e6ba46ac47c4f47967353a8c4bb4876e33cb3944
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-03-27 14:21:05 +0100

    Add function for printing status of resources

--- models/world_engine.py
+++ models/world_engine.py
@@ -23,3 +23,22 @@ class WorldEngine:
             for resource_label, quantity in entity.available_resources.items():
                 if quantity < 0:
                     print(f"Warning: Negative stockpile of {resource_label} in entity {entity.label}. Adjusting...")
+
+    def print_resources_table(self):
+        # Collect all unique resources from all entities
+        all_resources = set()
+        for entity in self.entities:
+            all_resources.update(entity.available_resources.keys())
+
+        # Start with the header row
+        table = "Resource".ljust(20) + "".join(entity.label.ljust(20) for entity in self.entities) + "\n"
+
+        # For each resource, create a row
+        for resource in sorted(all_resources):
+            row = resource.ljust(20)  # Resource name
+            for entity in self.entities:
+                # Add resource amount or "-" if the entity doesn't have it
+                row += str(entity.available_resources.get(resource, "-")).ljust(20)
+            table += row + "\n"
+
+        print(table)
